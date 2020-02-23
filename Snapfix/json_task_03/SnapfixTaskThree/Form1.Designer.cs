fffa64e00dd66db12183cbd02a66855cefe562fb
budd-priyanka <budd.priyanka@gmail.com>
2020-02-23 17:18:56 +0000

    Initial Submission
    Consists of the documentation and the code base.

---
+++ Snapfix/json_task_03/SnapfixTaskThree/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace SnapfixTaskThree
+{
+    partial class snapfixTaskThree
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(snapfixTaskThree));
+            this.headingLabel = new System.Windows.Forms.Label();
+            this.instructionLabel = new System.Windows.Forms.Label();
+            this.clickMeButton = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.idLabel = new System.Windows.Forms.Label();
+            this.detailsPanel = new System.Windows.Forms.Panel();
+            this.dataLabel = new System.Windows.Forms.Label();
+            this.infoLabel = new System.Windows.Forms.Label();
+            this.refreshButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.detailsPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // headingLabel
+            // 
+            this.headingLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.headingLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.headingLabel.Location = new System.Drawing.Point(657, 155);
+            this.headingLabel.Name = "headingLabel";
+            this.headingLabel.Size = new System.Drawing.Size(509, 36);
+            this.headingLabel.TabIndex = 0;
+            this.headingLabel.Text = "User Devices Information Provider";
+            // 
+            // instructionLabel
+            // 
+            this.instructionLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.instructionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.instructionLabel.Location = new System.Drawing.Point(707, 265);
+            this.instructionLabel.Name = "instructionLabel";
+            this.instructionLabel.Size = new System.Drawing.Size(542, 29);
+            this.instructionLabel.TabIndex = 1;
+            this.instructionLabel.Text = "Click on the below button to get the details";
+            // 
+            // clickMeButton
+            // 
+            this.clickMeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.clickMeButton.Enabled = false;
+            this.clickMeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clickMeButton.Location = new System.Drawing.Point(831, 327);
+            this.clickMeButton.Name = "clickMeButton";
+            this.clickMeButton.Size = new System.Drawing.Size(137, 46);
+            this.clickMeButton.TabIndex = 2;
+            this.clickMeButton.Text = "Click Me";
+            this.clickMeButton.UseVisualStyleBackColor = true;
+            this.clickMeButton.Click += new System.EventHandler(this.ClickMeButton_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox.ErrorImage = global::SnapfixTaskThree.Properties.Resources.SnapfixPicture;
+            this.pictureBox.Image = global::SnapfixTaskThree.Properties.Resources.SnapfixPicture;
+            this.pictureBox.InitialImage = global::SnapfixTaskThree.Properties.Resources.SnapfixPicture;
+            this.pictureBox.Location = new System.Drawing.Point(38, 153);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(443, 245);
+            this.pictureBox.TabIndex = 3;
+            this.pictureBox.TabStop = false;
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Location = new System.Drawing.Point(855, 207);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(100, 26);
+            this.idTextBox.TabIndex = 1;
+            this.idTextBox.TextChanged += new System.EventHandler(this.IdTextBox_TextChanged);
+            // 
+            // idLabel
+            // 
+            this.idLabel.AutoSize = true;
+            this.idLabel.Location = new System.Drawing.Point(818, 212);
+            this.idLabel.Name = "idLabel";
+            this.idLabel.Size = new System.Drawing.Size(30, 20);
+            this.idLabel.TabIndex = 6;
+            this.idLabel.Text = "ID:";
+            // 
+            // detailsPanel
+            // 
+            this.detailsPanel.Controls.Add(this.dataLabel);
+            this.detailsPanel.Controls.Add(this.infoLabel);
+            this.detailsPanel.Controls.Add(this.refreshButton);
+            this.detailsPanel.Location = new System.Drawing.Point(668, 254);
+            this.detailsPanel.Name = "detailsPanel";
+            this.detailsPanel.Size = new System.Drawing.Size(453, 313);
+            this.detailsPanel.TabIndex = 8;
+            this.detailsPanel.Visible = false;
+            // 
+            // dataLabel
+            // 
+            this.dataLabel.AutoSize = true;
+            this.dataLabel.Location = new System.Drawing.Point(17, 69);
+            this.dataLabel.Name = "dataLabel";
+            this.dataLabel.Size = new System.Drawing.Size(120, 20);
+            this.dataLabel.TabIndex = 9;
+            this.dataLabel.Text = "**Device Data**";
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.infoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infoLabel.Location = new System.Drawing.Point(11, 14);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(425, 85);
+            this.infoLabel.TabIndex = 9;
+            this.infoLabel.Text = "Device Information:";
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Enabled = false;
+            this.refreshButton.Location = new System.Drawing.Point(14, 239);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(87, 35);
+            this.refreshButton.TabIndex = 3;
+            this.refreshButton.Text = "Refresh";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.RefreshButton_Click);
+            // 
+            // snapfixTaskThree
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1358, 731);
+            this.Controls.Add(this.detailsPanel);
+            this.Controls.Add(this.clickMeButton);
+            this.Controls.Add(this.idLabel);
+            this.Controls.Add(this.idTextBox);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.instructionLabel);
+            this.Controls.Add(this.headingLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "snapfixTaskThree";
+            this.Text = "Snapfix Task  3";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.detailsPanel.ResumeLayout(false);
+            this.detailsPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label headingLabel;
+        private System.Windows.Forms.Label instructionLabel;
+        private System.Windows.Forms.Button clickMeButton;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.Label idLabel;
+        private System.Windows.Forms.Panel detailsPanel;
+        private System.Windows.Forms.Label dataLabel;
+        private System.Windows.Forms.Label infoLabel;
+        private System.Windows.Forms.Button refreshButton;
+    }
+}
+
---
+++ Snapfix/json_task_03/SnapfixTaskThree/Form1.cs
@@ -0,0 +1,126 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Windows.Forms;
+
+namespace SnapfixTaskThree
+{
+    public partial class snapfixTaskThree : Form
+    {
+        public snapfixTaskThree()
+        {
+            InitializeComponent();
+        }
+
+        // Declaring constants.
+        string URL_PREFIX = "https://live.documents.snapfix.io/sw_analyst/";
+        string NAME_TEXT = "Name: ";
+        string ADDRESS_TEXT = "Address: ";
+        string DEVICES_TEXT = "Devices: ";
+        string DEVICE_LIST_TEXT = "No device information available.";
+        string MODEL_TEXT = "Model: ";
+        string MANUF_TEXT = ", Manufacturer: ";
+        string YEAR_TEXT = ", Year: ";
+        string TABSPACE = ".  \t";
+        string NEWLINE = "\n";
+        string NAME_KEY = "name";
+        string ADDRESS_KEY = "address";
+        string DEVICES_KEY = "devices";
+        string MANUF_KEY = "manuf";
+        string MODEL_KEY = "model";
+        string YEAR_KEY = "year";
+
+        /* 
+         * This method makes the request to the server which returns a JSON.
+         * The JSON is parsed and the output is displayed in the details panel.
+         * An error message is shown in case of request failure.
+         */
+        private bool MakeRequest(string idText)
+        {
+            string displayDetails = "";
+            try
+            {
+                string urlPostfix = "user_" + idText + ".json";
+                WebRequest request = WebRequest.Create(URL_PREFIX + urlPostfix);
+                WebResponse response = request.GetResponse();
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream);
+                    string serverResponse = reader.ReadToEnd();
+                    JObject jsonResponse = JObject.Parse(serverResponse);
+
+                    //Creating details to be displayed
+                    displayDetails += NAME_TEXT + jsonResponse[NAME_KEY] + NEWLINE;
+                    displayDetails += ADDRESS_TEXT + jsonResponse[ADDRESS_KEY] + NEWLINE;
+                    displayDetails += DEVICES_TEXT + NEWLINE;
+                    if (jsonResponse[DEVICES_KEY].Count() > 0)
+                    {
+                        DEVICE_LIST_TEXT = "";
+                        for(int i = 0; i < jsonResponse[DEVICES_KEY].Count(); i++)
+                        {
+                            DEVICE_LIST_TEXT += (i + 1).ToString()
+                                + TABSPACE
+                                + MODEL_TEXT
+                                + jsonResponse[DEVICES_KEY][i][MODEL_KEY]
+                                + MANUF_TEXT
+                                + jsonResponse[DEVICES_KEY][i][MANUF_KEY]
+                                + YEAR_TEXT
+                                + jsonResponse[DEVICES_KEY][i][YEAR_KEY]
+                                + NEWLINE;
+                        }
+                        displayDetails += DEVICE_LIST_TEXT;
+                    }
+                    else
+                    {
+                        displayDetails += DEVICE_LIST_TEXT;
+                    }
+                    dataLabel.Text = displayDetails;
+                }
+                response.Close();
+                return true;
+            }
+            catch
+            {
+                MessageBox.Show("Failed to fetch details.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
+        // Actions performed on button clicks 
+        private void clickBehaviour(string idText)
+        {
+            if (idText.Length > 0)
+            {
+                if (MakeRequest(idText))
+                {
+                    detailsPanel.Visible = true;
+                    refreshButton.Enabled = true;
+                }
+                else
+                {
+                    detailsPanel.Visible = false;
+                }
+            }
+        }
+
+        private void ClickMeButton_Click(object sender, EventArgs e)
+        {
+            clickBehaviour(idTextBox.Text);
+        }
+
+        private void RefreshButton_Click(object sender, EventArgs e)
+        {
+            clickBehaviour(idTextBox.Text);
+        }
+
+        // Disable the buttons for blank ID field.
+        private void IdTextBox_TextChanged(object sender, EventArgs e)
+        {
+            bool idHasValue = (idTextBox.Text.Length > 0);
+            clickMeButton.Enabled = idHasValue;
+            refreshButton.Enabled = idHasValue;
+        }
+    }
+}
